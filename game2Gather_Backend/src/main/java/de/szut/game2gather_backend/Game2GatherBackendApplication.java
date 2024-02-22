302ed50efbc12a8f7128055d3fa297dfefbdad8d
lam <marc.lankau@hec.de>
2024-02-22 13:15:56 +0100

    initial setup

---
+++ game2Gather_Backend/src/main/java/de/szut/game2gather_backend/Game2GatherBackendApplication.java
@@ -0,0 +1,13 @@
+package de.szut.game2gather_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game2GatherBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Game2GatherBackendApplication.class, args);
+	}
+
+}
---
+++ game2Gather_Backend/src/test/java/de/szut/game2gather_backend/Game2GatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.szut.game2gather_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Game2GatherBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

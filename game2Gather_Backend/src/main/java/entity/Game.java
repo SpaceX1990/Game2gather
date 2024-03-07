4b72b18e87c485dc211768599ee0ba08d508c23f
dua <dua@neusta.de>
2024-03-07 10:32:48 +0100

    add game, tag and comment classes

---
+++ game2Gather_Backend/src/main/java/entity/Game.java
@@ -0,0 +1,40 @@
+package entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="Game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NonNull
+    private String title;
+    @NonNull
+    private int minPlayer;
+    @Nullable
+    private int maxPlayer;
+    @Nullable
+    @ManyToMany
+    @JoinTable(
+            name = "game_tags",
+            joinColumns = @JoinColumn(name = "game_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )    private Set<Tag> tags;
+    @Nullable
+    private String genre;
+    @Nullable
+    private String imagePath;
+    @Nullable
+    @OneToMany (mappedBy="game")
+    private List<Comment> comments = new ArrayList<>();
+}

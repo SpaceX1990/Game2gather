6affd3b2d850e71b5a7d1e2b4780e68a63dae398
VTischer <viktor.t1@schule.bremen.de>
2024-03-28 12:04:16 +0100

    GameService erweitert

--- game2Gather_Backend/src/main/java/de/szut/game2gather_backend/service/GameService.java
+++ game2Gather_Backend/src/main/java/de/szut/game2gather_backend/service/GameService.java
@@ -6,12 +6,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class GameService {
 
     private final GameRepository repository;
+
+    public Optional<Game> read(int id) {
+        return repository.findById(id);
+    }
 
     public List<Game> readAll() {
         return repository.findAll();

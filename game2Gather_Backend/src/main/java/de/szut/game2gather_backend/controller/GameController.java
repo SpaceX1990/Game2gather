0bb977692c7d40a2185fb4dadeb96ff88f67d04c
VTischer <viktor.t1@schule.bremen.de>
2024-03-28 12:15:29 +0100

    GameController erweitert.

--- game2Gather_Backend/src/main/java/de/szut/game2gather_backend/controller/GameController.java
+++ game2Gather_Backend/src/main/java/de/szut/game2gather_backend/controller/GameController.java
@@ -4,10 +4,12 @@ import de.szut.game2gather_backend.entity.Game;
 import de.szut.game2gather_backend.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/game")
@@ -15,6 +17,11 @@ import java.util.List;
 public class GameController {
 
     private final GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public Optional<Game> getGame(@PathVariable("id") int id) {
+        return gameService.read(id);
+    }
 
     @GetMapping()
     public List<Game> getAll() {

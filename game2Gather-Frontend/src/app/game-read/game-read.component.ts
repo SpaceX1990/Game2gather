ad5b1dfd20a53c2c991d91b69c021c099391b990
VTischer <viktor.t1@schule.bremen.de>
2024-04-03 08:48:12 +0200

    game-read komponente

---
+++ game2Gather-Frontend/src/app/game-read/game-read.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-read',
+  templateUrl: './game-read.component.html',
+  styleUrl: './game-read.component.scss'
+})
+export class GameReadComponent {
+
+}

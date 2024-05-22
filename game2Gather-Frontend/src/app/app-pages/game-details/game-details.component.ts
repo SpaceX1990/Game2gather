967c8c8890dab27dd727917a2dc249494c382539
VTischer <viktor.t1@schule.bremen.de>
2024-05-22 18:16:41 +0200

    Genre wird korrekt angezeigt

--- game2Gather-Frontend/src/app/app-pages/game-details/game-details.component.ts
+++ game2Gather-Frontend/src/app/app-pages/game-details/game-details.component.ts
@@ -1,60 +1,47 @@
 import { Component } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {Router} from "@angular/router";
-import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
-import {GameApiService} from "../../service/game-api.service";
-import {TagService} from "../../service/tag.service";
-import {GenreService} from "../../service/genre.service";
-import {HttpClient} from "@angular/common/http";
-import {TagModel} from "../../models/tag.model";
-import {GameModel} from "../../models/game.model";
-import { TagModule } from 'primeng/tag';
+import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
+import { FormControl, FormGroup } from "@angular/forms";
+import { GameApiService } from "../../service/game-api.service";
+import { GameModel } from "../../models/game.model";
 
 @Component({
-  selector: 'app-game-details',
-  templateUrl: './game-details.component.html',
-  styleUrl: './game-details.component.scss'
+    selector: 'app-game-details',
+    templateUrl: './game-details.component.html',
+    styleUrls: ['./game-details.component.scss']
 })
-export class GameDetailsComponent{
+export class GameDetailsComponent {
+    public gameForm: FormGroup | undefined;
+    public gameId: number | undefined;
+    public game: GameModel | undefined;
 
-  // tagsOptions: any[] | undefined = [];
-  // genresOptions: string | undefined;
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private gameApiService: GameApiService,
+    ) {
+        this.gameId = parseInt(route.snapshot.paramMap.get('game-id')!, 10);
+        this.readGame();
+    }
 
-  public gameForm: FormGroup | undefined;
-  public gameId: number | undefined;
-  public game: GameModel |undefined;
-  constructor(private readonly _fb: FormBuilder,
-              private router: Router,
-              private route: ActivatedRoute,
-              private gameApiService: GameApiService,
-              private tagService: TagService,
-              private genreService: GenreService,
-              private http: HttpClient,
-              public tag: TagModule) {
-    // this.tagsOptions = this.game?.tags;
-    // this.genresOptions = this.game?.genre;
-    this.gameId =parseInt(route.snapshot.paramMap.get('game-id')!);
-    this.readGame();
-  }
+    readGame() {
+        this.gameApiService.readGame(this.gameId).subscribe(game => {
+            this.game = game;
+            this.setGameForm(game);
+        });
+    }
 
-  readGame() {
-    this.gameApiService.readGame(this.gameId).subscribe(game => {
-      this.game = game;
-      this.setGameForm(game);
-    })
-  }
+    private setGameForm(game: GameModel) {
+        this.gameForm = new FormGroup({
+            title: new FormControl(game.title),
+            minPlayer: new FormControl(game.minPlayer),
+            maxPlayer: new FormControl(game.maxPlayer),
+            tags: new FormControl(game.tags),
+            genre: new FormControl(game.genre.label)
+        });
+    }
 
-  private setGameForm(game: GameModel) {
-    this.gameForm = new FormGroup({
-      title: new FormControl(game.title),
-      minPlayer: new FormControl(game.minPlayer),
-      maxPlayer: new FormControl(game.maxPlayer),
-      tags: new FormControl(game.tags),
-      genre: new FormControl(game.genre)
-    })
-  }
-
-  navToMainMenu() {
-    this.router.navigateByUrl('/spielesammlung');
-  }
+    routeToOverview() {
+        this.router.navigateByUrl('/spielesammlung');
+    }
 }
